e287549cca6d71483beedd185bb66e90f81a6c5c
Sean <43484827+DoctorOum@users.noreply.github.com>
2022-08-14 21:36:12 -0400

    Added model from old project. Got player moving with anims

--- HalloweenGame/Assets/Scripts/Player/PlayerController.cs
+++ HalloweenGame/Assets/Scripts/Player/PlayerController.cs
@@ -11,10 +11,13 @@ public class PlayerController : MonoBehaviour
 
     public GameObject PixelRenderCam;
 
+    public Animator animator;
+
     private void Awake()
     {
         playerInput = new PlayerInputs();
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         playerInput.UI.SwapPixelCamera.performed += _ => SwapCameras();
     }
 
@@ -30,8 +33,40 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        //Gets input from player and converts into a vector
         Vector2 direction = playerInput.PlayerMovement.Move.ReadValue<Vector2>();
         rb.velocity = new Vector3(direction.x, 0, direction.y) * playerSpeed;
+        direction = direction.normalized;
+
+        //if moving rotate character
+        if (rb.velocity.magnitude >= 0.1)
+        {
+            animator.SetBool("isMoving", true);
+            //Handles directionals
+            if (direction.x != 0 && direction.y == 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, direction.x * -90, 0));
+            }
+            else if(direction.y != 0 && direction.x == 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, (direction.y + 1) * 90, 0));
+            }
+            //Handles diagonals
+            else if(direction.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, -90 + direction.y * -45 , 0));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 90 + direction.y * 45, 0));
+            }
+            //transform.rotation = Quaternion.Euler(new Vector3(0, (direction.y > 0 ? direction.y * 180 : 0) + (direction.x < 0 ? 90 : direction.x > 0 ? -90 : 0 ), 0));
+            //transform.rotation = Quaternion.Euler(new Vector3(0, Vector3.Dot(new Vector3(direction.x,0,0),new Vector3(0, direction.y, 0)))) ;
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 
     void SwapCameras()
